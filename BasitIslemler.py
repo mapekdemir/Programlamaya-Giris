55ad177c04cc243c997b7ab582cd0c95ee0d0fd2
mapekdemir <72753036+mapekdemir@users.noreply.github.com>
2020-10-12 18:55:40 +0300

    BasitIslem.py
    @mapekdemir tarafından kodlanmıştır...

---
+++ BasitIslemler.py
@@ -0,0 +1,8 @@
+sayi1 = int(input("Lutfen birinci sayiyi giriniz: "))
+sayi2 = int(input("Lutfen ikinci sayiyi giriniz: "))
+print("toplam: ", sayi1 + sayi2)
+print("fark: ", sayi1 - sayi2)
+print("carpım: ", sayi1 * sayi2)
+print("bolum: ", sayi1 / sayi2)
+print("kalan: ", sayi1 // sayi2)
+#@mapekdemir
